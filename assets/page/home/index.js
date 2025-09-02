d61021e26d30214a6e7b7ad1272ca5a635e3d1c9
RADIOdemon6-alt <abyzaryws@gmail.com>
2025-09-02 12:17:53 +0300

    Create index.js

---
+++ assets/page/home/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./home.js"; // لو عندك كومبوننت رئيسي
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
