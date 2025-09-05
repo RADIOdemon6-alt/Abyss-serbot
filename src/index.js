6f4859b5f95f3b231fac08d41a72672ccb76c997
RADIOdemon6-alt <abyzaryws@gmail.com>
2025-09-05 14:02:56 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import MainApp from "./MainApp"; // اسم الملف الجديد
+import MainApp from "./AuthForm"; // اسم الملف الجديد
 import "./App.css";
 
 const container = document.getElementById("root");
 const root = createRoot(container);
-root.render(<MainApp />);
+root.render(<AuthForm />);
