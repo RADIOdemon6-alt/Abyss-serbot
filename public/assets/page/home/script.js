c8622f612a8a9fc4645f2ed88f39f9c9f75bd27a
RADIOdemon6-alt <abyzaryws@gmail.com>
2025-09-05 13:00:46 +0300

    Update script.js

--- public/assets/page/home/script.js
+++ public/assets/page/home/script.js
@@ -1,1 +1,19 @@
+// زر القائمة
+const menuBtn = document.getElementById("menu-btn");
+const sidebar = document.getElementById("sidebar");
 
+menuBtn.addEventListener("click", () => {
+  sidebar.classList.toggle("active");
+  menuBtn.classList.toggle("active"); // bars → X
+});
+
+// تشغيل الأنيميشن للـ Progress Bars
+window.addEventListener("load", () => {
+  const progresses = document.querySelectorAll(".progress");
+  progresses.forEach(bar => {
+    const value = bar.getAttribute("data-value");
+    setTimeout(() => {
+      bar.style.width = value + "%";
+    }, 300);
+  });
+});
