149deb0e514578352b6985e27d55afab590b5dc1
RADIOdemon6-alt <abyzaryws@gmail.com>
2025-09-05 16:38:43 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-importimport React from "react";
+import React from "react";
 import ReactDOM from "react-dom/client";
 import Router from "./router";
 
@@ -7,11 +7,4 @@ root.render(
   <React.StrictMode>
     <Router />
   </React.StrictMode>
-); React from "react";
-import { createRoot } from "react-dom/client";
-import MainApp from "./AuthForm"; // اسم الملف الجديد
-import "./App.css";
-
-const container = document.getElementById("root");
-const root = createRoot(container);
-root.render(<AuthForm />);
+);
