3196a3c2d58eb47b7a0e86f0f0200879d5a0a300
RADIOdemon6-alt <abyzaryws@gmail.com>
2025-09-05 16:54:05 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import Router from "./app";
+import Router from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <app />
+    <App />
   </React.StrictMode>
 );
